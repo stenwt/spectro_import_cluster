b88ff9f72be5e16702087b0a042d97efe4fa7976
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2021-12-13 08:28:39 -0700

    updates to aws variables

--- variables.tf
+++ variables.tf
@@ -2,9 +2,13 @@
 #Cloud Account
 #Variables used to create or reference a cloud account in Palette
 variable "cluster_cloud_account_name" {}
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_key_pair" {}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {
+  sensitive    = true
+}
+variable "aws_key_pair" {
+  description  = "your ssh public key"
+}
 
 #Cluster Profile
 variable "cluster_profile_name" {}
@@ -30,4 +34,6 @@ variable "master-pool_node_count" {
 variable "worker-pool_node_count" {
   default = "1"
 }
-variable "aws_ssh_key_name" {}
+variable "aws_ssh_key_name" {
+  description  = "name of your key pair for the AWS region you are operating in"
+}
