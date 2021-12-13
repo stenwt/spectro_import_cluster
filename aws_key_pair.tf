09eab10e0caabd74bc358fd9fbcbf4ad2c56f1f0
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2021-12-13 08:40:33 -0700

    removed provider stanza to provider.tf

--- aws_key_pair.tf
+++ aws_key_pair.tf
@@ -1,9 +1,4 @@
 
-# Configure the AWS Provider
-provider "aws" {
-  region = var.aws_region
-}
-
 # Create a key pair, Spectro Cloud Palette requires an existing key pair that will be injected into the ec2 instances it creates
 resource "aws_key_pair" "spectro_cloud_key" {
   key_name   = "spectro-key"
